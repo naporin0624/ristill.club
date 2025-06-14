c6cccf6cc3dbd27d798b787612035ca81b81d87e
naporin0624 <naporin0624@gmail.com>
2025-06-14 22:06:46 +0000

    chore: remove ogp.jpg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
-
-import ogp from "@assets/ogp.jpg";
-
 import { HeroSection } from "./_components/hero-section";
 
 const Home = () => {
 	return (
 		<main>
 			<HeroSection />
-			<Image src={ogp} alt="Hero Section" width={1920} height={1080} />
 		</main>
 	);
 };
